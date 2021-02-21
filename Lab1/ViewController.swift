39825fb209ed59c74c4251c52b8325e369b3bdd8
Vadstr <StriletskiyVadim@gmail.com>
2021-02-21 14:06:39 +0200

    Initial Commit

---
+++ Lab1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab1
+//
+//  Created by Vadim on 21.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
